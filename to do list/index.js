fae5aa7d4fea612d94105e7eb5f6a095d0d229c4
harish-karthik-2008 <harishkarthik817@gmail.com>
2025-10-08 09:30:42 +0530

    Create index.js

---
+++ to do list/index.js
@@ -0,0 +1,67 @@
+.nav{
+    width: 1547px;
+    margin-top: -21px;
+    margin-left: -20px;
+    height: 100px;
+    background-color: lightcoral;
+  }
+  body{
+    background-color:lightgray;
+}
+h1{
+    padding: 20px 0px 0px 600px;
+    color: white;
+}
+.completed {
+    text-decoration: line-through;
+    color: gray;
+    }
+.input{
+    width: 400px;
+    height: 30px;
+    margin-left: 400px;
+    margin-top: 100px;
+    border-radius: 10px;
+}
+.bt{
+    width: 100px;
+    height: 35px;
+    border-radius: 10px;
+    background-color: lightcoral;
+}
+.bt:hover{
+    background-color: lightblue;
+}
+hr{
+    margin-top: 200px;
+    border: 1px solid black;
+}
+#del{
+    width: 100px;
+    height: 30px;
+    float:right;
+    background-color: red;
+    border-radius: 10px;
+}
+#del:hover{
+    background-color: rgba(255, 81, 0, 0.777);
+}
+#box{
+    width: 70px;
+    height: 30px;
+    margin-top: 0px;
+    border-radius: 10px;
+    background-color: aquamarine;
+}
+.date{
+    width: 150px;
+    height: 30px;
+    border-radius: 10px;
+}
+.box{
+  width: 800px;
+  height: 150px;
+  background-color: antiquewhite;
+  padding: 30px;
+  border-radius: 10px;
+}
